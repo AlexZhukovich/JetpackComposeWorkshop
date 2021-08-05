c5475ac4555dd3a710b6067591917af58c26194d
Alex Zhukovich <zhukovich.a@gmail.com>
2021-08-05 08:49:18 +0200

    Added UiState class

---
+++ app/src/main/java/com/alexzh/jetpackcomposeworkshop/ui/common/UiState.kt
@@ -0,0 +1,10 @@
+package com.alexzh.jetpackcomposeworkshop.ui.common
+
+sealed class UiState<out T: Any?> {
+
+    object Loading : UiState<Nothing>()
+
+    data class Success<out T: Any>(val data: T) : UiState<T>()
+
+    data class Error(val exception: Exception) : UiState<Nothing>()
+}
